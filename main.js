739b57b52d121606a78cc0b941c925301f1f0743
NaneWoF <nanedj@gmail.com>
2025-07-06 23:14:39 -0400

    main.js

--- main.js
+++ main.js
@@ -621,7 +621,6 @@ if ("serviceWorker" in navigator) {
   navigator.serviceWorker.register("/WOF/firebase-messaging-sw.js")
     .then((registration) => {
       console.log("Service Worker registrado:", registration);
-      messaging.useServiceWorker(registration);
     })
     .catch((err) => {
       console.error("Error registrando Service Worker:", err);
